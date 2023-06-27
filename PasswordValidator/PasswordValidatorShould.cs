b245280b34a896605e3be20d94261c1f70579d2d
Rubén Zamora <ruben.zamora@leanmind.es>
2023-06-27 22:56:12 +0100

    ✅ green - Contains_At_Least_An_Lowercase

--- PasswordValidator/PasswordValidatorShould.cs
+++ PasswordValidator/PasswordValidatorShould.cs
@@ -2,9 +2,28 @@ namespace PasswordValidator.Unit.Tests;
 
 public class PasswordValidatorShould
 {
+    
     [Fact]
-    public void HaveMoreThan8Characters()
+    public void Be_Valid()
     {
-        PasswordValidator.IsInvalid("12345678").Should().BeFalse();
+        PasswordValidator.IsValid("Abc1234_").Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Have_More_Than_8_Characters()
+    {
+        PasswordValidator.IsValid("12345678").Should().BeFalse();
+    }
+    
+    [Fact]
+    public void Contains_At_Least_An_Uppercase()
+    {
+        PasswordValidator.IsValid("R").Should().BeFalse();
+    }
+    
+    [Fact]
+    public void Contains_At_Least_An_Lowercase()
+    {
+        PasswordValidator.IsValid("r").Should().BeFalse();
     }
 }
--- src/PasswordValidator/PasswordValidator/PasswordValidator.cs
+++ src/PasswordValidator/PasswordValidator/PasswordValidator.cs
@@ -2,8 +2,10 @@ namespace PasswordValidator;
 
 public class PasswordValidator
 {
-    public static bool IsInvalid(string password)
+    public static bool IsValid(string password)
     {
-        return password.Length < 8;
+        return password.Length < 8 
+               && password.Any(char.IsUpper)
+               && password.Any(char.IsLower);
     }
 }
